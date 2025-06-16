f94d2e96855e816a126a79f2259d23ac01c9abcb
Vesselin Petrunov <petrunov@protonmail.com>
2025-06-16 14:03:45 +0300

    Mail sending working on prod

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,11 +1,13 @@
 from .base import *
-
-# Development overrides
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
 # Console email backend for dev
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'noreply@localhost'
+# EMAIL_HOST           = env('EMAIL_HOST')
+# EMAIL_PORT           = int(env('EMAIL_PORT'))
+# EMAIL_HOST_USER      = env('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD  = env('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS        = env('EMAIL_USE_TLS')
+EMAIL_BACKEND        = env('EMAIL_BACKEND')
+DEFAULT_FROM_EMAIL   = env('DEFAULT_FROM_EMAIL')
+
 
 # Enable CORS for your React frontend
 INSTALLED_APPS += ['corsheaders']
--- config/settings/production.py
+++ config/settings/production.py
@@ -8,20 +8,21 @@ DATABASES = {
         "ENGINE":   "django.db.backends.mysql",
         "NAME":     env('DATABASE_NAME'),
         "USER":     env('DATABASE_USER'),
-        "PASSWORD": env('DATABASE_PASSWORD'),
+        "PASSWORD": '%$S$Qz-=+8bV&uDH', #env('DATABASE_PASSWORD'),
         "HOST":     env('DATABASE_HOST', default='localhost'),
         "PORT":     env('DATABASE_PORT', default='3306'),
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
-}Z
+}
 
 # Use real SMTP settings in production
-# EMAIL_BACKEND        = os.environ['EMAIL_BACKEND']
-# EMAIL_HOST           = os.environ['EMAIL_HOST']
-# EMAIL_PORT           = int(os.environ.get('EMAIL_PORT', 587))
-# EMAIL_HOST_USER      = os.environ['EMAIL_HOST_USER']
-# EMAIL_HOST_PASSWORD  = os.environ['EMAIL_HOST_PASSWORD']
-# EMAIL_USE_TLS        = os.environ.get('EMAIL_USE_TLS') == 'True'
-# DEFAULT_FROM_EMAIL   = os.environ['DEFAULT_FROM_EMAIL']
+EMAIL_BACKEND        = env('EMAIL_BACKEND')
+EMAIL_HOST           = env('EMAIL_HOST')
+EMAIL_PORT           = int(env('EMAIL_PORT'))
+EMAIL_HOST_USER      = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD  = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS        = env.bool('EMAIL_USE_TLS')
+EMAIL_USE_SSL        = env.bool('EMAIL_USE_SSL')
+DEFAULT_FROM_EMAIL   = env('DEFAULT_FROM_EMAIL')
